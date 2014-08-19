debd88543ebb9be156d5b0b7f800a2befae6ca20
getong <getonga@qq.com>
2014-08-19 15:09:39 +0800

    make it work for the recent erlang

--- dumpd.erl
+++ dumpd.erl
@@ -11,10 +11,10 @@
 %	]).
 %
 -define(DEFAULT_TCPOPTS, [
-	{verify, 0},
-	{active, false},
-	{certfile, "/Users/wolf/Projects/ThoughtExperiments/cert.pem"}
-	]).
+		{verify, 0},
+		{active, false},
+		{certfile, "cert.pem"}
+		]).
 
 
 -define(DEFAULT_PORT, 443).
@@ -23,7 +23,8 @@
 start()->
 	application:start(crypto),
 	application:start(ssl),
-	ssl:seed("rttflagdfttidppohjeh"),
+	ssl:start(),
+	%	ssl:seed("rttflagdfttidppohjeh"),
 	listen(true).
 
 % Call echo:listen(Port) to start the service.
@@ -41,51 +42,52 @@ listen(Port, true) ->
 	listen(Port, ?DEFAULT_TCPOPTS, true).
 
 listen(Port, TcpOpts, false = UseSSL) ->
-    {ok, LSocket} = gen_tcp:listen(Port, TcpOpts),
-    io:format("Now listening on port ~p, using following TCP options: ~p, and SSL flag set to ~p", [Port, TcpOpts, UseSSL]),
-    accept(LSocket);
+	{ok, LSocket} = gen_tcp:listen(Port, TcpOpts),
+	io:format("Now listening on port ~p, using following TCP options: ~p, and SSL flag set to ~p", [Port, TcpOpts, UseSSL]),
+	accept(LSocket);
 listen(Port, TcpOpts, true = UseSSL) ->
-    io:format("Attempting to use SSL"),
-    case ssl:listen(Port, TcpOpts) of
-	{ok, LSocket} ->
-    		io:format("Now listening on port ~p, using following TCP options: ~p, and SSL flag set to ~p", [Port, TcpOpts, UseSSL]),
-    		ssl_accept(LSocket);
-	{error, Reason} ->
-		io:format("ssl:listen returned error code of ~p", [Reason])
-   end.
+	io:format("Attempting to use SSL"),
+	case ssl:listen(Port, TcpOpts) of
+		{ok, LSocket} ->
+			io:format("Now listening on port ~p, using following TCP options: ~p, and SSL flag set to ~p", [Port, TcpOpts, UseSSL]),
+			ssl_accept(LSocket);
+		{error, Reason} ->
+			io:format("ssl:listen returned error code of ~p", [Reason])
+	end.
 
 
 
 % Wait for incoming connections and spawn the echo loop when we get one.
 accept(LSocket) ->
-    {ok, Socket} = gen_tcp:accept(LSocket),
-    spawn(fun() -> loop(Socket, gen_tcp) end),
-    accept(LSocket).
+	{ok, Socket} = gen_tcp:accept(LSocket),
+	spawn(fun() -> loop(Socket, gen_tcp) end),
+	accept(LSocket).
 
 ssl_accept(LSocket) ->
-    case ssl:transport_accept(LSocket) of
-    	{ok, TSocket} -> case ssl:ssl_accept(TSocket) of
-    		ok ->
-    			spawn(fun() -> loop(TSocket, ssl) end),
-    			ssl_accept(LSocket);
-		SslErr -> 
-			Msg = io_lib:format("Error, ssl:ssl_accept returned ~p", [SslErr]),
+	case ssl:transport_accept(LSocket) of
+		{ok, TSocket} -> 
+			case ssl:ssl_accept(TSocket) of
+				ok ->
+					spawn(fun() -> loop(TSocket, ssl) end),
+					ssl_accept(LSocket);
+				SslErr -> 
+					Msg = io_lib:format("Error, ssl:ssl_accept returned ~p", [SslErr]),
+					error_logger:error_msg(Msg),
+					exit(Msg)
+			end;
+		TransportErr -> 
+			Msg = io_lib:format("Error, ssl:transport_accept returned ~p", [TransportErr]),
 			error_logger:error_msg(Msg),
 			exit(Msg)
-		end;
-	TransportErr -> 
-		Msg = io_lib:format("Error, ssl:transport_accept returned ~p", [TransportErr]),
-		error_logger:error_msg(Msg),
-		exit(Msg)
 	end.
 
 % Echo back whatever data we receive on Socket.
 loop(Socket, Mod) ->
-    case Mod:recv(Socket, 0) of
-        {ok, Data} ->
-	    io:format("Socket received data...~n~p~n", [Data]),
-            loop(Socket, Mod);
-        {error, closed} ->
-	    io:format("Socket closed."),
-            ok
-    end.
+	case Mod:recv(Socket, 0) of
+		{ok, Data} ->
+			io:format("Socket received data...~n~p~n", [Data]),
+			loop(Socket, Mod);
+		{error, closed} ->
+			io:format("Socket closed."),
+			ok
+	end.
